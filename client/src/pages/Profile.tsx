335bc9ae98126297773ee6fa7abc69ce9058a42c
Shadowsdepth <Shadowsdepth22@gmail.com>
2025-04-01 22:08:12 -0600

    cleanup and a fix with a query

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,15 +1,14 @@
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+
 import AnimalList from '../components/AnimalList/index.tsx';
-
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-
+import { QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+  const { loading, data } = useQuery(QUERY_ME, {
     variables: { username: userParam },
   });
 
