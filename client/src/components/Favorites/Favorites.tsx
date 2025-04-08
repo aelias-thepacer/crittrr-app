af6bd5ed2716c8fb683cd97e35c3f38f218aeedd
aelias-thepacer <aelias.thepacer@gmail.com>
2025-04-07 19:30:44 -0600

    fixed build error

--- client/src/components/Favorites/Favorites.tsx
+++ client/src/components/Favorites/Favorites.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AnimalType } from '../../interfaces/AnimalType';
 
 function Favorites() {
